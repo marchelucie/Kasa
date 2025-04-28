c9885b89e6dea28631b87042176ef6d7aeb4cfee
marchelucie <lucie.pouponneau@live.fr>
2025-04-28 12:10:48 +0200

    Collapse content : p for text, ul for list

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import ArrowOpen from "../assets/arrow-open.svg";
 import ArrowClosed from "../assets/arrow-closed.svg";
-import "../styles/collapse.scss";
+import "../styles/Collapse.scss";
 
-function Collapse ({ title, text }) {
+function Collapse({ title, text }) {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleCollapse = () => {
@@ -14,10 +14,21 @@ function Collapse ({ title, text }) {
         <div className="collapse">
             <div className="collapse-header" onClick={toggleCollapse}>
                 <h2>{title}</h2>
-                <img src={isOpen ? ArrowOpen : ArrowClosed}/>
+                <img src={isOpen ? ArrowOpen : ArrowClosed} />
             </div>
-            {isOpen && <div className="collapse-content">{text}</div>}
-        </div>
+            {isOpen && (
+                <div className="collapse-content">
+                    {Array.isArray(text) ? (
+                        <ul>
+                            {text.map((item, index) => (
+                                <li key={index}>{item}</li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>{text}</p>
+                    )}
+                </div>
+            )}        </div>
     );
 }
 

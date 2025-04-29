4a2797cd1e3ff0f24f51ef1ba6a8f61c881bc584
marchelucie <lucie.pouponneau@live.fr>
2025-04-29 11:52:52 +0200

    404 page done

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,8 +1,14 @@
+import "../styles/Error404.scss";
+import { Link } from "react-router-dom";
 
 function Error() {
     return (
         <>
-            Erreur 404
+            <div className="error404">
+                <h1>404</h1>
+                <p className="error404_text">Oups! La page que vous demandez n'existe pas.</p>
+                <Link to="/"><p className="error404_return">Retourner sur la page d'accueil</p></Link>
+            </div>
         </>
     )
 }

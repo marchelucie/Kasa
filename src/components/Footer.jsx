eec1926bee0648ea64729a9cd270b92ff3eb4b34
marchelucie <lucie.pouponneau@live.fr>
2025-04-29 11:52:35 +0200

    added Link to return to Homepage by clicking on Kasa logo
    + replaced <a> by Link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import WhiteLogo from '../assets/white-logo.svg';
 import '../styles/Footer.scss';
 
 function Footer() {
     return (
         <footer>
+            <Link to="/">
             <img src={WhiteLogo} alt="Logo Kasa"/>
+            </Link>
             <p>© 2025 Kasa. All rights reserved</p>
         </footer>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
 import Logo from '../assets/logo.svg';
 import '../styles/Header.scss';
 
 function Header() {
     return (
         <header>
-            <img src={Logo} alt="Logo Kasa"/>
+            <Link to="/">
+                <img src={Logo} alt="Logo Kasa" />
+            </Link>
             <nav>
                 <ul className="nav-list">
-                    <li className="nav-item"><a href="/">Accueil</a></li>
-                    <li className="nav-item"><a href="/about">A Propos</a></li>
+                    <li className="nav-item"><Link to="/">Accueil</Link></li>
+                    <li className="nav-item"><Link to="/about">A Propos</Link></li>
                 </ul>
             </nav>
         </header>

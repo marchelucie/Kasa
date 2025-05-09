4d3ec64151780b14f1e7fafd7738a014a45f154a
marchelucie <lucie.pouponneau@live.fr>
2025-05-09 10:46:42 +0200

    Update Carousel.jsx
    UX : if the accomodation contains only 1 picture, there are no more arrow buttons nor counter

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -9,14 +9,14 @@ function Carousel({ images }) {
 
     return (
         <div className="carousel">
-            <div className='carousel-button-prev'>
+            {images.length > 1 ? (<div className='carousel-button-prev'>
                 <img src={Prev} alt="Previous" onClick={() => currentIndex - 1 < 0 ? setCurrentIndex(images.length - 1) : setCurrentIndex(currentIndex - 1)} />
-            </div>
+            </div>) : null}
             <img src={image} alt={`Image n° ${currentIndex + 1}`} className='carousel-image' />
-            <div className='carousel-button-next'>
+            {images.length > 1 ? (<div className='carousel-button-next'>
                 <img src={Next} alt="Next" onClick={() => currentIndex + 1 < images.length ? setCurrentIndex(currentIndex + 1) : setCurrentIndex(0)} />
-            </div>
-            <div className='images-index'><p>{currentIndex + 1}/{images.length}</p></div>
+            </div>) : null}
+            {images.length > 1 ? (<div className='images-index'><p>{currentIndex + 1}/{images.length}</p></div>) : null}
         </div>
     );
 }
